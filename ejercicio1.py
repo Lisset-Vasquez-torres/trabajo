fff620f4d91c4ffc4eda7e3dd96dc949f5362996
Lisset-Vasquez-torres <57965788+lisset-vasquez-torres@users.noreply.github.com>
2019-11-19 16:26:32 -0500

    Se agrego el primer ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,444 @@
+#Informacion_Boleta_de_Ventas
+#DECLARACIONES
+Libro_de_Audotoria_Tributaria=0.0
+libro_manual_del_contador=0.0
+compendio_laboral=0.0
+total=0.0
+#INPUT
+Libro_de_Audotoria_Tributaria=250.00
+libro_manual_del_contador=250.00
+compendio_laboral=300.00
+#PROCESSING
+total=(Libro_de_Audotoria_Tributaria+libro_manual_del_contador+compendio_laboral)
+#OUTPUT
+print ("######################")
+print ("#  BOLETA DE VENTAS  #")
+print ("#####################")
+print ("Libro de Audotoria Tributaria:",Libro_de_Audotoria_Tributaria)
+print ("libro manual del contador:",libro_manual_del_contador)
+print ("compendio laboral:",compendio_laboral)
+print ("Total:",total)
+print ("#####################")
+
+
+
+#Informacion_Gastos_de Operacion
+#DECLARACIONES
+gastos_de_operacion_05=0.0
+gastos_de_operacion_06=0.0
+gastos_de_operacion_07=0.0
+#INPUT
+gastos_de_operacion_05=3.563
+gastos_de_operacion_06=3.655
+gastos_de_operacion_07=4.130
+#PROCESSING
+total=(gastos_de_operacion_05+gastos_de_operacion_06+gastos_de_operacion_07)
+#OUTPUT
+print ("#############################")
+print ("# GASTOS DE OPERACION #")
+print ("#######################")
+print ("gastos de operacion 05:",gastos_de_operacion_05)
+print ("gastos de operacion 06:",gastos_de_operacion_06)
+print ("gastos de operacion 07:",gastos_de_operacion_07)
+print ("Total:",total)
+print ("#######################")
+
+
+#Informacion_costo_de_Ventas
+#DECLARACIONES
+costos_de_ventas_05=0.0
+costos_de_ventas_06=0.0
+costos_de_ventas_07=0.0
+#INPUT
+costos_de_ventas_05=9.271
+costos_de_ventas_06=11.649
+costos_de_ventas_07=13.868
+#PROCCESING
+total=(costos_de_ventas_05+costos_de_ventas_06+costos_de_ventas_07)
+#OUTPUT
+print ("####################")
+print ("# COSTOS DE VENTAS #")
+print ("####################")
+print ("costos de ventas 05:",costos_de_ventas_05)
+print ("costos de ventas 06:",costos_de_ventas_06)
+print ("costos de ventas 07:",costos_de_ventas_07)
+print ("total:",total)
+print ("####################")
+
+
+#Informacion_Total_activos
+#DECLARACIONES
+total_activos_05=0.0
+total_activos_06=0.0
+total_activos_07=0.0
+#INPUT
+total_activos_05=26.763
+total_activos_06=29.972
+total_activos_07=49.662
+#PROCCESING
+total=(total_activos_05+total_activos_06+total_activos_07)
+#OUTPUT
+print ("######################")
+print ("# TOTAL ACTIVOS #")
+print ("#################")
+print ("total activos 05:",total_activos_05)
+print ("total activos 06:",total_activos_06)
+print ("total activos 07:",total_activos_07)
+print ("total:",total)
+print ("#################")
+
+
+#Informacion_de_Inmuebles
+#DECLARACIONES
+inmuebles_05=0.0
+inmuebles_06=0.0
+inmuebles_07=0.0
+#INPUT
+inmuebles_05=15.542
+inmuebles_06=17.196
+inmuebles_07=22.895
+#PROCCESING
+total=(inmuebles_05+inmuebles_06+inmuebles_07)
+#OUTPUT
+print ("#############")
+print ("# INMUEBLES #")
+print ("#############")
+print ("inmuebles 05:",inmuebles_05)
+print ("inmuebles 06:",inmuebles_06)
+print ("inmuebles 07:",inmuebles_07)
+print ("total:",total)
+print ("#############")
+
+
+#Informacion_Total_Pasivo
+#DECLARACIONES
+total_pasivo_05=0.0
+total_pasivo_06=0.0
+total_pasivo_07=0.0
+#INPUT
+total_pasivo_05=16.409
+total_pasivo_06=15.193
+total_pasivo_07=30.967
+#PROCCESING
+total=(total_pasivo_05+total_pasivo_06+total_pasivo_07)
+#OUTPUT
+print ("################")
+print ("# TOTAL PASIVO #")
+print ("################")
+print ("total pasivo 05:",total_pasivo_05)
+print ("total pasivo 06:",total_pasivo_06)
+print ("total pasivo 07:",total_pasivo_07)
+print ("total:",total)
+print ("################")
+
+
+#Informacion_Total_de_Capital_contable
+#DECLARACIONES
+total_de_capital_contable_05=0.0
+total_de_capital_contable_06=0.0
+total_de_capital_contable_07=0.0
+#INPUT
+total_de_capital_contable_05=10.354
+total_de_capital_contable_06=14.779
+total_de_capital_contable_07=18.695
+#PROCCESING
+total=(total_de_capital_contable_05+total_de_capital_contable_06+total_de_capital_contable_07)
+#OUTPUT
+print ("#############################")
+print ("# TOTAL DE CAPITAL CONTABLE #")
+print ("#############################")
+print ("total de capital contable 05:",total_de_capital_contable_05)
+print ("total de capital contable 06:",total_de_capital_contable_06)
+print ("total de capital contable 07:",total_de_capital_contable_07)
+print ("total:",total)
+print ("#############################")
+
+
+#Informacion_Capital_de_Trabajo
+#DECLARACIONES
+capital_de_trabajo_05=0.0
+capital_de_trabajo_06=0.0
+capital_de_trabajo_07=0.0
+#INPUT
+capital_de_trabajo_05=1.268
+capital_de_trabajo_06=887
+capital_de_trabajo_07=1.383
+#PROCCESING
+total=(capital_de_trabajo_05+capital_de_trabajo_06+capital_de_trabajo_07)
+#OUTPUT
+print ("######################")
+print ("# CAPITAL DE TRABAJO #")
+print ("######################")
+print ("capital de trabajo 05:",capital_de_trabajo_05)
+print ("capital de trabajo 06:",capital_de_trabajo_06)
+print ("capital de trabajo 07:",capital_de_trabajo_07)
+print ("total:",total)
+print ("######################")
+
+
+#Informacion_Ventas_Netas
+#DECLARACIONES
+ventas_netas_05=0.0
+ventas_netas_06=0.0
+ventas_netas_07=0.0
+#INPUT
+ventas_netas_05=15.321
+ventas_netas_06=18.249
+ventas_netas_07=20.893
+#PROCCESING
+total=(ventas_netas_05+ventas_netas_06+ventas_netas_07)
+#OUTPUT
+print ("############################")
+print ("#       VENTAS NETAS       #")
+print ("############################")
+print ("ventas netas 05:",ventas_netas_05)
+print ("ventas netas 06:",ventas_netas_06)
+print ("ventas netas 07:",ventas_netas_07)
+print ("total:",total)
+print ("#############################")
+
+
+#Informacion_de _Utilidad
+#DECLARACIONES
+informacion_de_utilidad_05=0.0
+informacion_de_utilidad_06=0.0
+informacion_de_utilidad_07=0.0
+#INPUT
+informacion_de_utilidad_05=0.31
+informacion_de_utilidad_06=0.33
+informacion_de_utilidad_07=0.32
+#PROCCESING
+total=(informacion_de_utilidad_05+informacion_de_utilidad_06+informacion_de_utilidad_07)
+#OUTPUT
+print ("############")
+print ("# UTILIDAD #")
+print ("############")
+print ("informacion de utilidad 05:",informacion_de_utilidad_05)
+print ("informacion de utilidad 06:",informacion_de_utilidad_06)
+print ("informacion de utilidad 07:",informacion_de_utilidad_07)
+print ("total:",total)
+print ("############")
+
+
+#Informacion_Valor_en_libros
+#DECLARACIONES
+valor_en_libros_05=0.0
+valor_en_libros_06=0.0
+valor_en_libros_07=0.0
+#INPUT
+valor_en_libros_05=1.39
+valor_en_libros_06=1.75
+valor_en_libros_07=1.99
+#PROCCESING
+total=(valor_en_libros_05+valor_en_libros_06+valor_en_libros_07)
+#OUTPUT
+print ("###################")
+print ("# VALOR EN LIBROS #")
+print ("###################")
+print ("valor en libro 05:",valor_en_libros_05)
+print ("valor en libro 06:",valor_en_libros_06)
+print ("valor en libro 07:",valor_en_libros_07)
+print ("total:",total)
+print ("##################")
+
+
+
+#Informacion_de_equivalentes_de_efectivo
+#DECLARACIONES
+equivalentes_de_efectivo_05=0
+equivalentes_de_efectivo_06=0.0
+equivalentes_de_efectivo_07=0
+#INPUT
+equivalentes_de_efectivo_05=601
+equivalentes_de_efectivo_06=1.579
+equivalentes_de_efectivo_07=743
+#PROCCESING
+total=(equivalentes_de_efectivo_05+equivalentes_de_efectivo_06+equivalentes_de_efectivo_07)
+#OUTPUT
+print ("############################")
+print ("# EQUIVALENTES DE EFECTIVO #")
+print ("############################")
+print ("equivalentes de efectivo 05:",equivalentes_de_efectivo_05)
+print ("equivalentes de efectivo 06:",equivalentes_de_efectivo_06)
+print ("equivalentes de efectivo 07:",equivalentes_de_efectivo_07)
+print ("total:",total)
+print ("###########################")
+
+
+
+#Informacion_Productos_financieros
+#DECLARACIONES
+productos_financieros_05=0
+productos_financieros_06=0
+productos_financieros_07=0
+#INPUT
+productos_financieros_05=765
+productos_financieros_06=462
+productos_financieros_07=900
+#PROCCESING
+total=(productos_financieros_05+productos_financieros_06+productos_financieros_07)
+#OUTPUT
+print ("#########################")
+print ("# PRODUCTOS FINANCIEROS #")
+print ("#########################")
+print ("productos financieros 05:",productos_financieros_05)
+print ("productos financieros 06:",productos_financieros_06)
+print ("productos financieros 07:",productos_financieros_07)
+print ("total:",total)
+print ("########################")
+
+
+
+#Informacion_de_Dividendos_por_CPO
+#DECLARACIONES
+dividendos_05=0.0
+dividendos_06=0.0
+dividendos_07=0.0
+#INPUT
+dividendos_05=0.06
+dividendos_06=0.09
+dividendos_07=0.08
+#PROCCESING
+total=(dividendos_05+dividendos_06+dividendos_07)
+#OUTPUT
+print ("########################")
+print ("#  DIVIDENDOS POR CPO  #")
+print ("########################")
+print ("dividendos por CPO 05:",dividendos_05)
+print ("dividendos por CPO 06:",dividendos_06)
+print ("dividendos por CPO 07:",dividendos_07)
+print ("total:",total)
+print ("########################")
+
+
+
+#Informacion_TOTAL_DE_LA_PARTICIPACION_CONTROLADORA
+#DECLARACIONES
+participacion_controladora_05=0.0
+participacion_controladora_06=0.0
+participacion_controladora_07=0.0
+#INPUT
+participacion_controladora_05=9.825
+participacion_controladora_06=12.859
+participacion_controladora_07=14.942
+#PROCCESING
+total=(participacion_controladora_05+participacion_controladora_06+participacion_controladora_07)
+#OUTPUT
+print ("##########################################")
+print ("# TOTAL DE LA PARTICIPACION CONTROLADORA #")
+print ("##########################################")
+print("total de la participacion controladora 05:",participacion_controladora_05)
+print ("total de la participacion controladora 06:",participacion_controladora_06)
+print ("total de la participacion controladora 07:",participacion_controladora_07)
+print ("total:",total)
+print ("##########################################")
+
+
+
+#Informacion_Utilidad_neta_de_la_participacion_controladora
+#DECLARACIONES
+utilidad_05=0.0
+utilidad_06=0.0
+utilidad_07=0.0
+#INPUT
+utilidad_05=2.112
+utilidad_06=2.378
+utilidad_07=2.391
+#PROCCESING
+total=(utilidad_05+utilidad_06+utilidad_07)
+#OUTPUT
+print ("#################################################")
+print ("# UTILIDAD NETA DE LA PARTICIPACION CONTROLADORA ")
+print ("#################################################")
+print ("utilidad neta de la participacion controladora 05:",utilidad_05)
+print ("utilidad neta de la participacion controladora 06:",utilidad_06)
+print ("utilidad neta de la participacion controladora 07:",utilidad_07)
+print ("total:",total)
+print ("#################################################")
+
+
+
+#Informacion_Operaciones_discontinuas
+#DECLARACIONES
+operaciones_discontinuas_07=0
+operaciones_discontinuas_08=0
+operaciones_discontinuas_09=0
+#INPUT
+operaciones_discontinuas_07=26
+operaciones_discontinuas_08=187
+operaciones_discontinuas_09=314
+#PROCCESING
+total=(operaciones_discontinuas_07+operaciones_discontinuas_08+operaciones_discontinuas_09)
+#OUTPUT
+print ("############################")
+print ("# OPERACIONES DISCONTINUAS #")
+print ("############################")
+print ("operaciones discontinuas 07:",operaciones_discontinuas_07)
+print ("operaciones discontinuas 08:",operaciones_discontinuas_08)
+print ("operaciones discontinuas 09:",operaciones_discontinuas_09)
+print ("total:",total)
+print ("############################")
+
+
+#Informacion_de_Gastos_Financieros
+#DECLARACIONES
+gastos_05=0
+gastos_06=0
+gastos_07=0
+#INPUT
+gastos_05=526
+gastos_06=494
+gastos_07=807
+#PROCCESING
+total=(gastos_05+gastos_06+gastos_07)
+#OUTPUT
+print ("#############################")
+print ("# GASTOS FINANCIEROS #")
+print ("######################")
+print ("gastos 05:",gastos_05)
+print ("gastos 06:",gastos_06)
+print ("gastos 07:",gastos_07)
+print ("total:",total)
+print ("######################")
+
+#Informacion_de_Participacion_no_controladora
+#DECLARACIONES
+participacion_05=0
+participacion_06=0
+participacion_07=0
+#INPUT
+participacion_05=55
+participacion_06=110
+participacion_07=77
+#PROCCESING
+total=(participacion_05+participacion_06+participacion_07)
+#OUTPUT
+print ("#################################")
+print ("# PARTICIPACION NO CONTROLADORA #")
+print ("#################################")
+print ("paricipacion 05:",participacion_05)
+print ("participacion 06:",participacion_06)
+print ("participacion 07:",participacion_07)
+print ("total:",total)
+print ("#################################")
+
+#Informacion_Utilidad_bruta
+#DECLARACIONES
+utilidad_bruta_05=0.0
+utilidad_bruta_06=0.0
+utilidad_bruta_07=0.0
+#INPUT
+utilidad_bruta_05=6.050
+utilidad_bruta_06=6.600
+utilidad_bruta_07=7.025
+#PROCCESING
+total=(utilidad_bruta_05+utilidad_bruta_06+utilidad_bruta_07)
+#OUTPUT
+print ("##################")
+print ("# UTILIDAD BRUTA #")
+print ("##################")
+print ("utilidad bruta 05:",utilidad_bruta_05)
+print ("utilidad bruta 06:",utilidad_bruta_06)
+print ("utilidad bruta 07:",utilidad_bruta_07)
+print ("total:",total)
+print ("##################")
